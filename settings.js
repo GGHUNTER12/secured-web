d2d15331d6e5111eed9a879beecb686d51e435c8
GGHUNTER12 <117963991+GGHUNTER12@users.noreply.github.com>
2025-02-17 00:28:53 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,35 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const body = document.body;
-    const logoLink = document.querySelector('.logo a');
     const storedBg = localStorage.getItem("customBackground");
-
-    function isGradient(bgValue) {
-        return bgValue.includes('gradient');
-    }
-
-    function extractColorFromGradient(gradient) {
-        const canvas = document.createElement('canvas');
-        canvas.width = 1;
-        canvas.height = 1;
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0, 0, 1, 1);
-        const pixelData = ctx.getImageData(0, 0, 1, 1).data;
-        return `rgb(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]})`;
-    }
 
     function applyStyles(bgValue) {
         body.style.background = bgValue;
+        document.documentElement.style.setProperty("--dynamic-color", bgValue);
         body.classList.remove("default-bg");
-
-        let dynamicColor;
-        if (isGradient(bgValue)) {
-            dynamicColor = extractColorFromGradient(bgValue);
-        } else {
-            dynamicColor = bgValue;
-        }
-
-        document.documentElement.style.setProperty("--dynamic-color", dynamicColor);
     }
 
     // Apply stored background if found
