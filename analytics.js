4c4cd468a6b0382c7bdcfbf75441ad8ef87555cf
GGHUNTER12 <117963991+GGHUNTER12@users.noreply.github.com>
2025-02-16 16:51:26 -0800

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,14 @@
+// analytics.js
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-Z5FC4ZV3MX';
+  document.head.appendChild(script);
+
+  script.onload = function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-Z5FC4ZV3MX');
+  };
+})();
