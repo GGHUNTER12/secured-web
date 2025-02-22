faa5c36944b1afbc53aceaf3492861f2c225dcfc
GGHUNTER12 <117963991+GGHUNTER12@users.noreply.github.com>
2025-02-22 10:38:17 -0800

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.getElementById("loading-screen").classList.add("hidden");
+    }, 1000); // Wait 1 second before hiding the loader
+});
